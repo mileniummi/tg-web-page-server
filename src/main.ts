db192abf076fb6c1df9f232d213f4ce71ab2246c
Mihail <kokosh2000@mail.ru>
2022-03-28 17:23:59 +0300

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('web page from tg')
     .setDescription('Документация REST API')
